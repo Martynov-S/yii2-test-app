1298ebcd2a212071afbdf497391c2ef55f532ec5
Sergey Martynov <sfan245l@mail.ru>
2025-05-13 14:02:16 +0300

    подготовка к публикации

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=tests',
-    'username' => 'wptest',
-    'password' => 'base-4WP-tests',
+    'username' =>'',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
